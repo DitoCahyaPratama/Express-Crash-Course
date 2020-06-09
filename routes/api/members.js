eff0f60cf7b9b640123a5120bf83ee6a65baefef
DitoCahyaPratama <ditocahyapratama717@gmail.com>
2020-06-10 06:19:57 +0700

    Add router - Update Member

--- routes/api/members.js
+++ routes/api/members.js
@@ -23,7 +23,7 @@ router.post("/", (req, res) => {
     id: uuid.v4(),
     name: req.body.name,
     email: req.body.email,
-    status: "active"
+    status: "active",
   };
 
   if (!newMember.name || !newMember.email) {
@@ -32,6 +32,25 @@ router.post("/", (req, res) => {
 
   members.push(newMember);
   res.json(members);
+});
+
+// Update Member
+router.put("/:id", (req, res) => {
+  const found = members.some((member) => member.id === parseInt(req.params.id));
+
+  if (found) {
+    const updMember = req.body;
+    members.forEach((member) => {
+      if (member.id === parseInt(req.params.id)) {
+        member.name = updMember.name ? updMember.name : member.name;
+        member.email = updMember.email ? updMember.email : member.email;
+      
+        res.json({ msg: 'Member updated', member });
+      }
+    });
+  } else {
+    res.status(400).json({ msg: `No member with the id of ${req.params.id}` });
+  }
 });
 
 module.exports = router;
