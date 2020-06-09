b8b2acd04d91cd8fd7d990bc955bb0fbaf03214d
DitoCahyaPratama <ditocahyapratama717@gmail.com>
2020-06-09 22:08:36 +0700

    Create Middleware

--- index.js
+++ index.js
@@ -1,6 +1,18 @@
-const express = require('express');
+const express = require("express");
+const path = require("path");
+const logger = require('./middleware/logger');
+const members = require("./Members");
 
 const app = express();
+
+// Init Middleware
+// app.use(logger);
+
+// Gets All Members
+app.get("/api/members", (req, res) => res.json(members));
+
+// Set static folder
+app.use(express.static(path.join(__dirname, "public")));
 
 const PORT = process.env.PORT || 5000;
 
